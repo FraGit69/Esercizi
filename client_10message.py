57abfb833ac295b7a920258f84f824cfd4e8c750
FraGit69 <francesco.re@itiscuneo.eu>
2024-09-20 13:33:36 +0200

    modifiche

--- client_10message.py
+++ client_10message.py
@@ -9,4 +9,4 @@ server_address = ("localhost", 6980)
 BUFFER_SIZE = 4092 #uanti bit posso inviare o ricevere
 
 for i in range(10):
-    udp_client_socket.sendto(f"messaggio numero: {i}", server_address)
+    udp_client_socket.sendto(f"messaggio numero: {i}".encode('utf-8'), server_address)
